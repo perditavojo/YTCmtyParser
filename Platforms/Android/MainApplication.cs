e80137e2b748bb5a109926a30040775d6e9ed89f
perditavojo <117562794+perditavojo@users.noreply.github.com>
2023-04-24 16:14:09 +0800

    feat: 套用數個異動
    1. 重新調整 Android 的權限相關、版本限制等設定。
    2. 重新調整應用程式圖檔的內容。
    3. 調整顯示的應用程式名稱。
    4. 針對 Android 平台，調整附件內容的顯示方式，以及加入權限請求相關程式碼。

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,7 +1,11 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
+//[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 29)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
 
 namespace YTCmtyParser;
